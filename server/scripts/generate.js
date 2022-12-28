f514c2e5102f72be2ef6d2551562b21633e0480e
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2022-12-28 20:27:41 +0000

    Created the public key

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -2,6 +2,10 @@ const secp = require("ethereum-cryptography/secp256k1");// this is the library t
 const { toHex } = require("ethereum-cryptography/utils");// this is the library that we will use to convert the private key to hex
 
 const privateKey = secp.utils.randomPrivateKey();// this is the function that generates the private key
-
+// console.log('private key:', privateKey);
 //Run into server: node scripts/generate.js to see the private key
-console.log('private key:', toHex(privateKey));// this is the function that converts the private key to hex  
+console.log('private key:', toHex(privateKey));// this is the function that converts the private key to hex
+
+const publicKey = secp.getPublicKey(privateKey);// this is the function that generates the public key
+
+console.log('public key:', toHex(publicKey)); // this is the function that converts the public key to hex
