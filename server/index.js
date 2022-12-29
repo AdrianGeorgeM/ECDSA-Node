c1fddadd328ab5f4bea0ba058a1daa08b6f31995
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2022-12-29 19:40:30 +0000

    Fixed the private key to show the public key

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "2afd95f3bb4792abc941437f3cf7efc254c15f76": 100,//this is the public key address  of the first account
-  "0af293a676cd5e3ced2ea9f81e3b5e5f5abdf5b9": 50,// this is the public key address  of the second account
-  "d2d48b7849547e1de38e85e5d92284df6167be16": 75, // this is the public key address  of the third account
+  "04f3e09f3a2264f96e610cb144d165ba66f604891fe641968b5832abc6b82145db167a30a007ea0936004603f5af4d24f0155f03c7ec3344e963d81ba611807285": 100,//this is the public key address  of the first account
+  "0468afdac4b8c264377956a20445ec2fc2cc8a3a4b13f90572dd8a338c1c5f1d47b514c11822cbcedbb8dfbdd01f6040bb8a2c966d0f748cd77bb8894250ad2a4c": 50,// this is the public key address  of the second account
+  "04c319b871a0ccb8e21312e8fbac685b865a9ed1949285aa771849d3a40f9932515159cc65e0905c924ef0f45cf24f0be37b18c8dc77b9dd9d9db627c0cc45fa2a": 75, // this is the public key address  of the third account
 };
 
 app.get("/balance/:address", (req, res) => {
--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,30 +1,46 @@
-const secp = require("ethereum-cryptography/secp256k1");// this is the library that we will use to generate the private key
-const { toHex } = require("ethereum-cryptography/utils");// this is the library that we will use to convert the private key to hex
-
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
 const { keccak256 } = require("ethereum-cryptography/keccak");
-const privateKey = secp.utils.randomPrivateKey();// this is the function that generates the private key
-// console.log('private key:', privateKey);
-//Run into server: node scripts/generate.js to see the private key
-console.log('private key:', toHex(privateKey));// this is the function that converts the private key to hex
 
-const publicKey = secp.getPublicKey(privateKey);// this is the function that generates the public key
+
+const privateKey = secp.utils.randomPrivateKey();
+
+console.log('private key:', toHex(privateKey));
+
+const publicKey = secp.getPublicKey(privateKey);
+console.log('public key:', toHex(publicKey));//long public key
+
 // take the last 20 bytes of the keccak hash and return this.
-console.log('public key:', toHex(publicKey));// this is the function that converts the public key to hex
-
 let publicK = publicKey.slice(1);
-const hash = keccak256(publicK);
-const address = keccak256(hash).slice(-20);
-console.log('public key:', toHex(address)); // this is the function that converts the public key to hex
+const hash = keccak256(publicK).slice(-20);
+console.log('public key:', toHex(hash)); //short public key
+// //end of the snippet
 
 
-//Take the last 20 bytes of the keccak hash and return this.
 
-// function getAddress(publicKey) {
-//     let publicK = publicKey.slice(1);
-//     const hash = keccak256(publicK);
-//     const address = keccak256(hash).slice(-20);
-//     return toHex(address);
-// }
+// const privateKey = secp.utils.randomPrivateKey();// this is the function that generates the private key
+// // console.log('private key:', privateKey);
+// //Run into server: node scripts/generate.js to see the private key
+// console.log('private key:', toHex(privateKey));// this is the function that converts the private key to hex
 
-// getAddress(publicKey);
+// const publicKey = secp.getPublicKey(privateKey);// this is the function that generates the public key
+// // take the last 20 bytes of the keccak hash and return this.
+// console.log('public key:', toHex(publicKey));// this is the function that converts the public key to hex
+
+// let publicK = publicKey.slice(1);
+// const hash = keccak256(publicK);
+// const address = keccak256(hash).slice(-20);
+// console.log('public key:', toHex(address)); // this is the function that converts the public key to hex
+
+
+// //Take the last 20 bytes of the keccak hash and return this.
+
+// // function getAddress(publicKey) {
+// //     let publicK = publicKey.slice(1);
+// //     const hash = keccak256(publicK);
+// //     const address = keccak256(hash).slice(-20);
+// //     return toHex(address);
+// // }
+
+// // getAddress(publicKey);
 
