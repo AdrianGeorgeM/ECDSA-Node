cc9e9f972f2ec95cd831ddded8f627c6d640e69a
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-04 22:05:35 +0000

    Working to refractor the code and implement better logic

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,54 +1,108 @@
 import { useState } from "react";
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
 
-function Transfer({ address, setBalance }) {
-  const [sendAmount, setSendAmount] = useState("");
-  const [recipient, setRecipient] = useState("");
+function Transfer({ address, setBalance, privateKey }) {
+	const [sendAmount, setSendAmount] = useState("");
+	const [recipient, setRecipient] = useState("");
+	const [signature, setSignature] = useState("");
+	const [recoveryBit, setRecoveryBit] = useState("");
 
-  const setValue = (setter) => (evt) => setter(evt.target.value);
+	const setValue = (setter) => (evt) => setter(evt.target.value);
 
-  async function transfer(evt) {
-    evt.preventDefault();
+	const hashMessage = (message) => {
+		const bytes = utf8ToBytes(message);
+		const hash = keccak256(bytes);
+		// console.log("hash:", toHex(hash));
+		return toHex(hash);
+	};
 
-    try {
-      const {
-        data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
-      setBalance(balance);
-    } catch (ex) {
-      alert(ex.response.data.message);
-    }
-  }
+	async function transfer(evt) {
+		evt.preventDefault();
 
-  return (
-    <form className="container transfer" onSubmit={transfer}>
-      <h1>Send Transaction</h1>
+		const message = {
+			address,
+			sendAmount,
+			recipient,
+		};
 
-      <label>
-        Send Amount
-        <input
-          placeholder="1, 2, 3..."
-          value={sendAmount}
-          onChange={setValue(setSendAmount)}
-        ></input>
-      </label>
+		const hash = hashMessage(JSON.stringify(message));
 
-      <label>
-        Recipient
-        <input
-          placeholder="Type an address, for example: 0x2"
-          value={recipient}
-          onChange={setValue(setRecipient)}
-        ></input>
-      </label>
+		const [sig, recoveryBit] = await secp.sign(hash, privateKey, {
+			recovered: true,
+		});
 
-      <input type="submit" className="button" value="Transfer" />
-    </form>
-  );
+		const signMessage = (hash, privateKey) => {
+			const messageHash = hashMessage(hash);
+			return secp.sign(messageHash, privateKey, { recovered: true });
+		};
+
+		const recoverKey = (message, signature, recoveryBit) => {
+			const messageHash = hashMessage(message);
+			// console.log(
+			// 	toHex(secp.recoverPublicKey(messageHash, signature, recoveryBit))
+			// );
+			return toHex(secp.recoverPublicKey(messageHash, signature, recoveryBit));
+		};
+
+		const verifySignature = (message, signature, publicKey) => {
+			const messageHash = hashMessage(message);
+			return secp.verifySignature(messageHash, signature, publicKey);
+		};
+
+		// hashMessage(message);
+		recoverKey(hash, sig, recoveryBit);
+		const signMessageV = signMessage(hash, privateKey);
+		message.sign = toHex(sig);
+		message.recoveryBit = recoveryBit;
+		setSignature(toHex(sig));
+		setRecoveryBit(recoveryBit);
+		console.log("MESSAGE:", message);
+		// console.log("recoverKey:", recoverKey(hash, sig, recoveryBit));
+		// console.log("signMessage:", signMessageV);
+
+		try {
+			const {
+				data: { balance },
+			} = await server.post(`send`, {
+				sender: address,
+				amount: parseInt(sendAmount),
+				recipient,
+			});
+			setBalance(balance);
+		} catch (ex) {
+			alert(ex.response.data.message);
+		}
+	}
+
+	return (
+		<form className='container transfer' onSubmit={transfer}>
+			<h1>Send Transaction</h1>
+
+			<label>
+				Send Amount
+				<input
+					placeholder='1, 2, 3...'
+					value={sendAmount}
+					onChange={setValue(setSendAmount)}
+				></input>
+			</label>
+
+			<label>
+				Recipient
+				<input
+					placeholder='Type an address, for example: 0x2'
+					value={recipient}
+					onChange={setValue(setRecipient)}
+				></input>
+			</label>
+			<div>Signature: {signature}</div>
+			<div>Recovery Bit: {recoveryBit}</div>
+			<input type='submit' className='button' value='Transfer' />
+		</form>
+	);
 }
 
 export default Transfer;
--- server/index.js
+++ server/index.js
@@ -7,40 +7,40 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "78667289ba046040d0b8030b43df0020bd60ab2b": 100,//private key: 4b7dac24309b95122db428cd86fd1d3933344d8bbb57f2187051fede961a2d7e
-  "b267700bae6b3b329034f11cd11b39331e77864a": 50,// private key: 13744f655b516d43f1044ffbc304082a8515b279a158b3ae7e2abb17d1903ff0
-  "19a92164c94e8f9b60938e208bdbc8c8c11c3b1e": 75, // private key: db5337b9f5dc15a94b0dfd6863cbf35feb279af520a1e60eff43164e1460739b
+	"78667289ba046040d0b8030b43df0020bd60ab2b": 100, //private key: 4b7dac24309b95122db428cd86fd1d3933344d8bbb57f2187051fede961a2d7e
+	"04614eb26b5091e9194dff3132d517ae4016f3fa8447a1b25569d10aab3d69a21bca815d07e4bc741349c52b1d4791a739a6836f5cdc4f49d20cdc5b6ff60d9bce": 520, // private key: 2b25e896ebce975ba7a8c6323d35bf66070cb1c42fa5a41594ef18c2ca2e7836
+	"19a92164c94e8f9b60938e208bdbc8c8c11c3b1e": 75, // private key: db5337b9f5dc15a94b0dfd6863cbf35feb279af520a1e60eff43164e1460739b
 };
 
 app.get("/balance/:address", (req, res) => {
-  const { address } = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
+	const { address } = req.params;
+	const balance = balances[address] || 0;
+	res.send({ balance });
 });
 
 app.post("/send", (req, res) => {
-  //TODO get a signature from the clien-side application
-  //recover the public key from the signature
-  const { sender, recipient, amount } = req.body;
+	//TODO get a signature from the clien-side application and
+	//recover the public key from the signature
+	const { sender, recipient, amount } = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+	setInitialBalance(sender);
+	setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
-  }
+	if (balances[sender] < amount) {
+		res.status(400).send({ message: "Not enough funds!" });
+	} else {
+		balances[sender] -= amount;
+		balances[recipient] += amount;
+		res.send({ balance: balances[sender] });
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+	console.log(`Listening on port ${port}!`);
 });
 
 function setInitialBalance(address) {
-  if (!balances[address]) {
-    balances[address] = 0;
-  }
+	if (!balances[address]) {
+		balances[address] = 0;
+	}
 }
